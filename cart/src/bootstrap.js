fd1511bd4c12cfdf2033a84e014f703bb1d0c61d
Alfio Martini <alfio.martini@gmail.com>
2022-07-10 15:26:02 -0300

    factor 'cart' to expose 'cartMount'

--- cart/src/bootstrap.js
+++ cart/src/bootstrap.js
@@ -2,4 +2,19 @@ import { faker } from "@faker-js/faker";
 
 const cartText = `<div>You have ${faker.random.numeric()} in your Cart</div>`;
 
-document.querySelector("#dev-cart").innerHTML = cartText;
+function cartMount(elm) {
+  elm.innerHTML = cartText;
+}
+
+// test if we are locally and in development mode
+
+if (process.env.NODE_ENV === "development") {
+  console.log("cart development mode");
+  const elm = document.querySelector("#dev-cart");
+  if (elm) {
+    cartMount(elm);
+    console.log("mounting cart message");
+  }
+}
+
+export { cartMount };
--- cart/webpack.config.js
+++ cart/webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
       name: "cart",
       filename: "remoteEntry.js",
       exposes: {
-        "./CartIndex": "./src/index",
+        "./CartIndex": "./src/bootstrap",
       },
       shared: ["@faker-js/faker"],
     }),
