359253463003355c7810f4006b6fb16e082000bf
Alfio Martini <alfio.martini@gmail.com>
2022-07-10 14:33:19 -0300

    factor 'products' to expose 'mount'

--- products/src/bootstrap.js
+++ products/src/bootstrap.js
@@ -1,10 +1,28 @@
 import { faker } from "@faker-js/faker";
 
-let products = "";
+function mount(el) {
+  let products = "";
 
-for (let i = 0; i < 6; i++) {
-  const name = faker.commerce.productName();
-  products += `<div>${name}</div>`;
+  for (let i = 0; i < 6; i++) {
+    const name = faker.commerce.productName();
+    products += `<div>${name}</div>`;
+  }
+
+  el.innerHTML = products;
 }
 
-document.querySelector("#dev-products").innerHTML = products;
+// 1) Context/Situation #1
+//    - We are running this file (index, products, cart) in isolation (development mode)
+//    - The local index.html file definitely has an id of 'dev-products' ('dev-cart)
+//    -  We definitely want to render our app (product, cart) into that element
+
+mount(document.querySelector("#dev-products"));
+
+// 2) Context/Situation #2
+//   - We are running this file in development or in production through the container
+//   app.
+//   - There is no guarantee that an element with an id of 'dev-products' ('dev-cart')
+//   exists in the index.html of container
+//   - We do not want to try to immediately render the app
+
+export { mount };
