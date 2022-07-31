84324ca973583a82aa7a249c860ba3273b54644d
Alfio Ricardo de Brito Martini <amartini@daitan.com>
2022-07-30 21:27:32 -0300

    fix 'exposes' property in webpack.config: products & cart

--- cart/webpack.config.js
+++ cart/webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
       name: "cart",
       filename: "remoteEntry.js",
       exposes: {
-        "./CartIndex": "./src/index",
+        "./CartIndex": "./src/bootstrap",
       },
       shared: ["@faker-js/faker"],
     }),
